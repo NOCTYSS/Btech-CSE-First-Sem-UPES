46b49f96ff0c59ff90b286266a806462e15acebb
Paras Rana <parasrana8806@gmail.com>
2025-08-31 13:58:59 +0530

    Leap year program

---
+++ leap.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+int year;
+bool isLeapYear(int year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }  
