bf1b2268333cb24fe6c1a0cc591712ca2981c599
Paras Rana <parasrana8806@gmail.com>
2025-09-01 12:57:23 +0530

    Finding whether the triangle is isoceles,equilateral or scalene by checking its three sides

---
+++ triangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int s1, s2, s3;
+    printf("Enter the lengths of the three sides of the triangle: ");
+    if (scanf("%d %d %d", &s1, &s2, &s3) != 3 || s1 <= 0 || s2 <= 0 || s3 <= 0) {
+        printf("Invalid input. Please enter positive integers.\n");
+        return 1;
+    }
+    // Triangle inequality check
+    if (s1 + s2 <= s3 || s2 + s3 <= s1 || s1 + s3 <= s2) {
+        printf("The given sides do not form a valid triangle.\n");
+        return 1;
+    }
+    if (s1 == s2 && s2 == s3) {
+        printf("The triangle is Equilateral.\n");
+    } else if (s1 == s2 || s2 == s3 || s1 == s3) {
+        printf("The triangle is Isosceles.\n");
+    } else {
+        printf("The triangle is Scalene.\n");
+    }
+    return 0;
+}
