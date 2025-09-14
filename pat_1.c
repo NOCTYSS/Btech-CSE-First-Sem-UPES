bf83e9dcd4f1aa5a02dc226700508389e7ae6b77
Paras Rana <parasrana8806@gmail.com>
2025-09-14 21:04:59 +0530

    Write a program to print the following pattern:

---
+++ pat_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+    while(n != 0)
+    {
+        for(int i =1;i<=5l;i++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        n--;
+    }
+    return 0;
+}
