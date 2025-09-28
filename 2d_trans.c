cb87ac64f195a1170690b871333e8d1dfc273a86
Paras Rana <parasrana8806@gmail.com>
2025-09-28 19:22:11 +0530

    Q74: Find the transpose of a matrix.

---
+++ 2d_trans.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int m,n;;
+    printf("Enter number of rows: ");
+    scanf("%d", &m);
+    printf("Enter number of columns: ");
+    scanf("%d", &n);
+
+    int matrix[m][n], transpose[n][m];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Find transpose
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    
+    printf("The original matrix is:\n");
+    for (int i = 0; i < m; i++) 
+    {
+        for (int j = 0; j < n; j++) 
+        {
+           printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    printf("Transpose of the matrix:\n");
+    for (int i = 0; i < n; i++) \
+    {
+        for (int j = 0; j < m; j++) 
+        {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
