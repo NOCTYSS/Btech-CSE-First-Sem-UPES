e85a1e2e456a8ac20ff806643635356182cbdfd3
Paras Rana <parasrana8806@gmail.com>
2025-10-02 18:38:49 +0530

    Q86: Check if a string is a palindrome.

---
+++ st_palin.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = 0;
+    while (str[len] != '\0' && str[len] != '\n') {
+        len++;
+    }
+
+    int is_palindrome = 1;
+    for (int i = 0; i < len / 2; i++) {
+        if (str[i] != str[len - 1 - i]) {
+            is_palindrome = 0;
+            break;
+        }
+    }
+
+    if (is_palindrome)
+        printf("Palindrome\n");
+    else
+        printf("Not palindrome\n");
+
+    return 0;
+}
