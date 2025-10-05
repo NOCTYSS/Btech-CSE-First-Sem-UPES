1ca7d9618d2876ed82e9b4848ed61f239802b897
Paras Rana <parasrana8806@gmail.com>
2025-10-05 12:55:44 +0530

    Q89: Count frequency of a given character in a string.

---
+++ st_freq.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], ch;
+    int freq = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Enter the character to count: ");
+    scanf("%c", &ch);
+
+    int i = 0;
+    while (str[i] != '\0' && str[i] != '\n') {
+        if (str[i] == ch)
+            freq++;
+        i++;
+    }
+
+    printf("%d\n", freq);
+    return 0;
+}
